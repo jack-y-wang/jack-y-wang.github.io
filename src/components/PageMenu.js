9938bedd053bfab94ca046b411b3b09d51dd7afb
Jack Wang <jack.y.wang@berkeley.edu>
2020-06-19 16:19:10 -0700

    fixed the menu

--- src/components/PageMenu.js
+++ src/components/PageMenu.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { Container, Menu, Image } from 'semantic-ui-react';
+import { Container, Menu, Image, Visibility } from 'semantic-ui-react';
 
-const PageMenu = () => {
-	return (
-		<Menu secondary borderless>
-			<Container text>
-				<Menu.Item>
-					<a href="/jackwang_resume.pdf">resume</a>
-				</Menu.Item>
-				<Menu.Item position="right">
-					<Image size="mini" src="./imgs/favicon.png" />
-				</Menu.Item>
-			</Container>
-		</Menu>
-	);
+const menuStyle = {
+	border: 'none',
+	borderRadius: 0,
+	boxShadow: 'none'
+	// marginBottom: '',
+	// marginTop: '4em',
+	// transition: 'box-shadow 0.5s ease, padding 0.5s ease'
 };
 
-export default PageMenu;
+export default class PageMenu extends React.Component {
+	state = {
+		barPassed: false
+	};
+
+	// stickBar = () => this.setState({ barPassed: true });
+	// unStickBar = () => this.setState({ barPassed: false });
+
+	render() {
+		const { barPassed } = this.state;
+
+		return (
+			<div>
+				<Menu borderless style={menuStyle} fixed="top">
+					<Container text={barPassed ? false : true} vertical={barPassed ? true : false}>
+						<Menu.Item>
+							<a href="#Academics">academics</a>
+						</Menu.Item>
+						<Menu.Item>
+							<a href="#Experience">experience</a>
+						</Menu.Item>
+						<Menu.Item>
+							<a href="/jackwang_resume.pdf">resume</a>
+						</Menu.Item>
+						<Menu.Item position="right">
+							<Image size="mini" src="./imgs/favicon.png" />
+						</Menu.Item>
+					</Container>
+				</Menu>
+			</div>
+		);
+	}
+}
