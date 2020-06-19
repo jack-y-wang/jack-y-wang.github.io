350c7830dd53fe5b72f4f7fd3486763cc0b22c63
Jack Wang <jack.y.wang@berkeley.edu>
2020-06-19 10:58:21 -0700

    added resume link

--- src/components/PageMenu.js
+++ src/components/PageMenu.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Container, Menu, Image } from 'semantic-ui-react'
+import React from 'react';
+import { Container, Menu, Image } from 'semantic-ui-react';
 
 const PageMenu = () => {
-  return (
-    <Menu secondary borderlexx fixed="top">
-      <Container text>
-        <Menu.Item as="a">resume</Menu.Item>
-        <Menu.Item position="right">
-          <Image size="mini" src="./imgs/favicon.png" />
-        </Menu.Item>
-      </Container>
-    </Menu>
-  )
-}
+	return (
+		<Menu secondary borderless>
+			<Container text>
+				<Menu.Item>
+					<a href="/jackwang_resume.pdf">resume</a>
+				</Menu.Item>
+				<Menu.Item position="right">
+					<Image size="mini" src="./imgs/favicon.png" />
+				</Menu.Item>
+			</Container>
+		</Menu>
+	);
+};
 
-export default PageMenu
+export default PageMenu;
