af7dc75943f966e423e82420030867331d9e23fe
Jack Wang <jack.y.wang@berkeley.edu>
2020-06-19 11:01:25 -0700

    changed browser icon and title

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import PageMenu from './components/PageMenu'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import PageMenu from './components/PageMenu';
+import HeaderIntro from './components/HeaderIntro';
+import AcademicSection from './components/AcademicSection';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<PageMenu />, document.getElementById('root'))
+ReactDOM.render(
+	<div>
+		<PageMenu />
+		<HeaderIntro />
+		<AcademicSection />
+	</div>,
+	document.getElementById('root')
+);
