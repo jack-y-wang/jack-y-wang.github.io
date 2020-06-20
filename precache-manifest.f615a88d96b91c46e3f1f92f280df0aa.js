d59beeed9e8517846e30337b5973e4f19aaa5e15
Jack Wang <jack.y.wang@berkeley.edu>
2020-06-19 21:05:47 -0700

    Updates

---
+++ precache-manifest.f615a88d96b91c46e3f1f92f280df0aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7eab8fd15c4e8747df236d1fe148d44",
+    "url": "//jack-y-wang.github.io/index.html"
+  },
+  {
+    "revision": "ba685d7f15a68afa8e65",
+    "url": "//jack-y-wang.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "687107b7959be2348354",
+    "url": "//jack-y-wang.github.io/static/js/2.de3a6fe2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "//jack-y-wang.github.io/static/js/2.de3a6fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba685d7f15a68afa8e65",
+    "url": "//jack-y-wang.github.io/static/js/main.cf5408ab.chunk.js"
+  },
+  {
+    "revision": "b34ced41cb9cb1b5c4b5",
+    "url": "//jack-y-wang.github.io/static/js/runtime-main.48d42a05.js"
+  }
+]);
